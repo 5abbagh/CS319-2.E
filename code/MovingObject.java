4c4811184beead5eb8157dc27b3f442269179ecc
mehmetfdogan <mehmetfdogan@hotmail.com.tr>
2017-05-01 09:20:28 +0300

    Update MovingObject.java

--- code/MovingObject.java
+++ code/MovingObject.java
@@ -1,15 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package mazerunner;
 
 import java.awt.Image;
 
 /**
  *
- * @author LUL
+ * @author Mehmet
  */
 public abstract class MovingObject extends GameObject{
     int speed;
