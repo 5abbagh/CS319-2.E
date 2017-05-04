ccfcca4d52c281b15a63be4b402678d54c010728
umitcanhasbi <umitcanhasbioglu@gmail.com>
2017-05-04 11:11:55 +0300

    Update filemanager.java

--- code/filemanager.java
+++ code/filemanager.java
@@ -1,10 +1,12 @@
+
 package mazerunner;
+import java.awt.Image;
 import java.util.ArrayList;
 
-public class filemanager
+public class filemngr
 {
-	private ArrayList<String> namelist = new ArrayList<String>; // initialize namelist
-	public  ArrayList<Image> imagelist = new ArrayList<Image>; // initialize image list
+	private ArrayList<String> namelist = new ArrayList<String>(); // initialize namelist
+	public  ArrayList<Image> imagelist = new ArrayList<Image>(); // initialize image list
 	
 	public ArrayList<Image> getImagelist() 
 	{
@@ -16,6 +18,38 @@ public class filemanager
 	    return namelist;
 	}
 	
+	public ArrayList<Image> setImagelist() 
+	{
+		this.imagelist = imagelist;
+	}
+	
+	public ArrayList<Image> setnamelist() 
+	{
+		this.namelist = namelist;
+	}
+	public static int readMaze(int numPlayers,int level)
+	{
+		int[][] abc = new int[2][3];
+		if(numPlayers == 1)
+		{
+			if(level == 1)
+				return abc[1][1];
+			else if(level==2 )
+				return abc[1][2];
+			else
+				return abc[1][3];	
+		}
+		else if(numPlayers == 1)
+		{
+			if(level == 2)
+				return abc[2][1];
+			else if(level==2 )
+				return abc[2][2];
+			else
+				return abc[2][3];	
+		}
+		
+	}
 	
 	
 	
