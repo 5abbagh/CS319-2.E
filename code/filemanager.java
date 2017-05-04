358f2591bb94383f0a932b4e969666273ca2a743
umitcanhasbi <umitcanhasbioglu@gmail.com>
2017-05-05 02:34:21 +0300

    Update filemanager.java

--- code/filemanager.java
+++ code/filemanager.java
@@ -1,3 +1,4 @@
+import java.awt.Image;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -10,16 +11,123 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Scanner;
-
+import javax.swing.ImageIcon;
 
 public class FileMngr
 {
-
-	public static ArrayList<String> namelist = new ArrayList<String>(); // initialize namelist
-	public static ArrayList<Integer> scorelist = new ArrayList<>(); // initialize score list
+	private static ArrayList<String> namelist;
+	public static ArrayList<Image> list = new ArrayList<Image>();
+	public static ArrayList<Image> getImages()
+	  {
+	      namelist = new ArrayList();
+	      namelist.add("bomb");
+	      namelist.add("expl");
+	      namelist.add("ai");
+	      namelist.add("ai2");
+	      namelist.add("right");
+	      namelist.add("left");
+	      namelist.add("up");
+	      namelist.add("down");
+	      namelist.add("life");
+	      namelist.add("score");
+	      namelist.add("speed");
+	      namelist.add("bonus");
+	    
+	      for(String s : namelist)
+	          list.add((new ImageIcon("src\\img\\" + s + ".png")).getImage());
+	      return list;
+	  }
 	
-	public ArrayList<String> getNames() throws FileNotFoundException
+	public static int[][] getMaze(int numP, int difficulty){
+		//2 for 1 start
+	    //3 for 1 end
+		//-2 for 2 start
+	    //-3 for 2 end
+	    //1 for walls
+		if(numP == 1 )
+		{
+			if(difficulty == 1)
+			{
+			    int a1[] = {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
+			    int[] a2 = {1,2,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,1};
+			    int[] a3 = {1,0,1,0,1,0,1,0,0,0,0,0,1,1,1,0,1,0,1};
+			    int[] a4 = {1,0,1,0,1,0,1,1,1,1,0,1,0,0,1,0,1,0,1};
+			    int[] a5 = {1,0,1,1,1,0,1,0,0,0,0,0,-2,0,1,1,1,0,1};
+		     	int[] a6 = {1,-3,0,0,0,0,1,0,1,0,1,1,0,0,0,0,0,3,1};
+			    int[] a7 = {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
+			    int[][] b1 = {a1, a2, a3, a4, a5, a6, a7};
+			    return b1;
+			}
+			else if(difficulty == 2)
+			{
+			    int a1[] = {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
+			    int[] a2 = {1,2,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,1};
+			    int[] a3 = {1,0,1,0,1,0,1,0,0,0,0,0,1,1,1,0,1,0,1};
+			    int[] a4 = {1,0,1,0,1,0,1,1,1,1,0,1,0,0,1,0,1,0,1};
+			    int[] a5 = {1,0,1,1,1,0,1,0,0,0,0,0,-2,0,1,1,1,0,1};
+		     	int[] a6 = {1,-3,0,0,0,0,1,0,1,0,1,1,0,0,0,0,0,3,1};
+			    int[] a7 = {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
+			    int[][] b1 = {a1, a2, a3, a4, a5, a6, a7};
+			    return b1;
+			}
+			else
+			{
+				int a1[] = {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
+			    int[] a2 = {1,2,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,1};
+			    int[] a3 = {1,0,1,0,1,0,1,0,0,0,0,0,1,1,1,0,1,0,1};
+			    int[] a4 = {1,0,1,0,1,0,1,1,1,1,0,1,0,0,1,0,1,0,1};
+			    int[] a5 = {1,0,1,1,1,0,1,0,0,0,0,0,-2,0,1,1,1,0,1};
+		     	int[] a6 = {1,-3,0,0,0,0,1,0,1,0,1,1,0,0,0,0,0,3,1};
+			    int[] a7 = {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
+			    int[][] b1 = {a1, a2, a3, a4, a5, a6, a7};
+			    return b1;
+			}
+		}
+		else 
+		{	
+			if(difficulty == 1)
+			{
+			    int a1[] = {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
+			    int[] a2 = {1,2,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,1};
+			    int[] a3 = {1,0,1,0,1,0,1,0,0,0,0,0,1,1,1,0,1,0,1};
+			    int[] a4 = {1,0,1,0,1,0,1,1,1,1,0,1,0,0,1,0,1,0,1};
+			    int[] a5 = {1,0,1,1,1,0,1,0,0,0,0,0,-2,0,1,1,1,0,1};
+		     	int[] a6 = {1,-3,0,0,0,0,1,0,1,0,1,1,0,0,0,0,0,3,1};
+			    int[] a7 = {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
+			    int[][] b1 = {a1, a2, a3, a4, a5, a6, a7};
+			    return b1;
+			}
+			else if(difficulty == 2)
+			{
+			    int a1[] = {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
+			    int[] a2 = {1,2,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,1};
+			    int[] a3 = {1,0,1,0,1,0,1,0,0,0,0,0,1,1,1,0,1,0,1};
+			    int[] a4 = {1,0,1,0,1,0,1,1,1,1,0,1,0,0,1,0,1,0,1};
+			    int[] a5 = {1,0,1,1,1,0,1,0,0,0,0,0,-2,0,1,1,1,0,1};
+		     	int[] a6 = {1,-3,0,0,0,0,1,0,1,0,1,1,0,0,0,0,0,3,1};
+			    int[] a7 = {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
+			    int[][] b1 = {a1, a2, a3, a4, a5, a6, a7};
+			    return b1;
+			}
+			else
+			{
+				int a1[] = {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
+			    int[] a2 = {1,2,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,1};
+			    int[] a3 = {1,0,1,0,1,0,1,0,0,0,0,0,1,1,1,0,1,0,1};
+			    int[] a4 = {1,0,1,0,1,0,1,1,1,1,0,1,0,0,1,0,1,0,1};
+			    int[] a5 = {1,0,1,1,1,0,1,0,0,0,0,0,-2,0,1,1,1,0,1};
+		     	int[] a6 = {1,-3,0,0,0,0,1,0,1,0,1,1,0,0,0,0,0,3,1};
+			    int[] a7 = {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
+			    int[][] b1 = {a1, a2, a3, a4, a5, a6, a7};
+			    return b1;
+			}
+		}
+			
+	  }
+	
+	public static ArrayList<String> getNames() throws FileNotFoundException
 	{
+		ArrayList<String> namelist = new ArrayList<String>(); // initialize namelist
 		Scanner fileIn = new Scanner(new File("/Users/umitcanhasbioglu/Desktop/names.txt"));
 		for(int i=0;i<5;i++)
 		{
@@ -29,8 +137,10 @@ public class FileMngr
 		return namelist;
 	}
 	
-	public ArrayList<String> getScores() throws FileNotFoundException
+	
+	public static ArrayList<Integer> getScores() throws FileNotFoundException
 	{
+		ArrayList<Integer> scorelist = new ArrayList<>(); // initialize score list
 		Scanner fileIn = new Scanner(new File("/Users/umitcanhasbioglu/Desktop/scores.txt"));
 		for(int i=0;i<5;i++)
 		{
@@ -38,9 +148,11 @@ public class FileMngr
 			int x = Integer.parseInt(s);
 			scorelist.add(x);
 		}
-		return namelist;
+		return scorelist;
 	}
-
+	
+	
+	
 	public static void replaceOldName(String oldScore,String name) throws IOException
 	{
 		int foo = Integer.parseInt(oldScore);
@@ -62,6 +174,7 @@ public class FileMngr
 			
 			}
 		}
+		
 	}
 	
 	
@@ -77,14 +190,6 @@ public class FileMngr
 
 		
 	}
-	public static void main(String[] args) throws IOException
-	{
-		saveHighScore("xxx","1243434","55");
-		System.out.println(namelist);
-		System.out.println(scorelist);
-		
-		
-	}
+	
 	
 }
-
