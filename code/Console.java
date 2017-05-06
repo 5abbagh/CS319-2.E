0eb8bb2dfb03a762dd165753e978178d4bc509dc
mehmetfdogan <mehmetfdogan@hotmail.com.tr>
2017-05-06 16:32:19 +0300

    Update Console.java

--- code/Console.java
+++ code/Console.java
@@ -4,43 +4,44 @@ import java.awt.Dimension;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class Console extends JFrame
 {
 	MainMenu mainMenu;     	// 0
 	Board board;			// 1 for single		// 2 for multi
-	//Leaderboard lb;		// 3
-	//Tutorial tut;			// 4
+	Leaderboard lb;		// 3
+	Tutorial tut;			// 4
 	int level = 1;
 	String [] titles;
 	JPanel [] panels;
+	int temp;
 	JPanel panel;
 	public Console()
 	{
 		
 		panel = new JPanel();
+		panel.addKeyListener(new PanelKeysAdapter());
 		mainMenu = new MainMenu( this );
-		//lb  = new Leaderboard( this);
-		//tut = new Tutorial();
+		lb  = new Leaderboard( this);
+		tut = new Tutorial();
 		titles = new String [] {"Main Menu", "Single Player", "Multiplayer", "Leaderboard", "Tutorials"};
 		panels = new JPanel [4];
-		
 		panels[0] = mainMenu;
+		panels[2] = lb;
+		panels[3] = tut;
 
-		//panels[2] = lb;
-		//panels[3] = tut;
-		//panel.add(lb);
 		add(panel);
                 
                 setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
                 this.setVisible(true);
-                selectOpt(0 , 1);
+                
                 setLocationRelativeTo(null);
                 this.setSize(new Dimension(1368,768));
                 this.setResizable(false);
-                
-                
+                setVisible(true);
+                selectOpt(0 , 1);
 	}
 	
         public JPanel getPanel(){
@@ -48,24 +49,28 @@ public class Console extends JFrame
         }
         
 	// Set panel
+    private int activeOp = 0;
 	public void selectOpt( int panel, int level)
 	{
+		this.panel.requestFocusInWindow();
 		if (panel == 0 )
 		{
+			activeOp = 0;
 			this.panel.removeAll();
 			board = null;
-			this.panel.add(panels [0]);
-			while(!panels[0].hasFocus())
-				panels[0].requestFocusInWindow();
+			mainMenu = new MainMenu(this);
+			panels[0] = mainMenu;
+			
+			this.panel.add(panels[0]);
 			this.panel.revalidate();
             this.revalidate();
 			setTitle(titles[0]);
             this.pack();
-            System.out.println("here");
+            
 		}
 		else if (panel == 1 )
 		{
-			
+			activeOp = 1;
 			this.panel.removeAll();
             board = new Board(1,level, this);            
 			this.panel.add(board);
@@ -76,7 +81,7 @@ public class Console extends JFrame
 		}
 		else if (panel == 2 )
 		{
-                                        
+			activeOp = 2;                            
 			this.panel.removeAll();
 			board = new Board(2,level, this);
 			this.panel.add(board);
@@ -87,10 +92,9 @@ public class Console extends JFrame
 		}
 		else if (panel == 3 )
 		{
+			activeOp = 3;
 			this.panel.removeAll();
 			this.panel.add(panels [2]);
-			while(!panels[2].hasFocus())
-				panels[2].requestFocusInWindow();
 			this.panel.revalidate();
             this.revalidate();
 			setTitle(titles[3]);
@@ -98,10 +102,9 @@ public class Console extends JFrame
 		}
 		else if (panel == 4 )
 		{
+			activeOp = 4;
 			this.panel.removeAll();
 			this.panel.add(panels [3]);
-			while(!panels[3].hasFocus())
-				panels[3].requestFocusInWindow();
 			this.panel.revalidate();
 			this.revalidate();
 			setTitle(titles[4]);
@@ -110,5 +113,46 @@ public class Console extends JFrame
 		repaint();
 	}
         
-
+	class PanelKeysAdapter extends KeyAdapter 
+    {
+		public void keyPressed(KeyEvent e){
+	    	switch(activeOp){
+	    	case 0:
+	    		mainMenu.keyPressed(e);
+	    		break;
+	    	case 1:
+	    		board.keyPressed(e);
+	    		break;
+	    	case 2:
+	    		board.keyPressed(e);
+	    		break;
+	    	case 3:
+	    		tut.keyPressed(e);
+	    		break;
+	    	case 4:
+	    		lb.keyPressed(e);
+	    		break;
+	    	}
+	    }
+	    
+	    public void keyReleased(KeyEvent e){
+	    	switch(activeOp){
+	    	case 0:
+	    		mainMenu.keyReleased(e);
+	    		break;
+	    	case 1:
+	    		board.keyReleased(e);
+	    		break;
+	    	case 2:
+	    		board.keyReleased(e);
+	    		break;
+	    	case 3:
+	    		tut.keyReleased(e);
+	    		break;
+	    	case 4:
+	    		lb.keyReleased(e);
+	    		break;
+	    	}
+	    }
+    }
 }
