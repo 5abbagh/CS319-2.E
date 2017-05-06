590d40739e1022b18d0251d03cffbd1a60bf083d
Ali Sabbagh <ali96sab@gmail.com>
2017-05-07 00:00:38 +0300

    Update MainMenu.java

--- code/MainMenu.java
+++ code/MainMenu.java
@@ -3,6 +3,7 @@ package mazerunner;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
@@ -10,6 +11,7 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.util.ArrayList;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
@@ -20,107 +22,102 @@ import mazerunner.BoardUI.MyKeysAdapter;
 
 import java.awt.Font;
 
-public class MainMenu extends JPanel
+public class MainMenu extends JPanel implements KeyListener
 {
 	
 	// Properties
 	Console console;
 	int labelSelected = 0;
-	int buttonSelected = 1;
+	int buttonSelected = 0;
 	int level;
 	ArrayList<JLabel> labels;
 	ArrayList<JButton> buttons;
-	private MyMouse keys = new MyMouse();
+	Image background;
 	
 	// Constructor
 	public MainMenu( Console cn)
 	{
-		//super();
-		
-		this.addKeyListener(new MyMouse());
 		console = cn;
 		this.setPreferredSize(new Dimension(1366,768));
-		setLayout(null);
-		level = 1;
+		level = 0;
 		labels = new ArrayList<JLabel>();
 		buttons = new ArrayList<JButton>();
+		background = new ImageIcon("src\\img\\" + "background" + ".jpg").getImage();
+		setLayout(null);
 		
 		JLabel singlePlayer = new JLabel("Single Player");
+		singlePlayer.setForeground(Color.WHITE);
+		singlePlayer.setBounds(605, 88, 172, 29);
 		singlePlayer.setFont(new Font("Tahoma", Font.PLAIN, 24));
 		singlePlayer.setHorizontalAlignment(SwingConstants.CENTER);
-		singlePlayer.setBounds(837, 211, 172, 29);
 		add(singlePlayer);
 		labels.add(singlePlayer);
-		//singlePlayer.addMouseListener(keys);
 		
 		JLabel multiplayer = new JLabel("Multiplayer");
+		multiplayer.setForeground(Color.WHITE);
+		multiplayer.setBounds(605, 146, 172, 29);
 		multiplayer.setHorizontalAlignment(SwingConstants.CENTER);
 		multiplayer.setFont(new Font("Tahoma", Font.PLAIN, 24));
-		multiplayer.setBounds(837, 281, 172, 29);
 		add(multiplayer);
 		labels.add(multiplayer);
-		//multiplayer.addMouseListener(keys);
 		
 		JLabel diff = new JLabel("Difficulty");
+		diff.setForeground(Color.WHITE);
+		diff.setBounds(334, 243, 172, 29);
 		diff.setHorizontalAlignment(SwingConstants.CENTER);
 		diff.setFont(new Font("Tahoma", Font.PLAIN, 24));
-		diff.setBounds(603, 365, 172, 29);
 		add(diff);
 		labels.add(diff);
-		//diff.addMouseListener(keys);
 		
 		JLabel lblLeaderboard = new JLabel("Leaderboard");
+		lblLeaderboard.setForeground(Color.WHITE);
+		lblLeaderboard.setBounds(603, 345, 172, 29);
 		lblLeaderboard.setHorizontalAlignment(SwingConstants.CENTER);
 		lblLeaderboard.setFont(new Font("Tahoma", Font.PLAIN, 24));
-		lblLeaderboard.setBounds(837, 473, 172, 29);
 		add(lblLeaderboard);
 		labels.add(lblLeaderboard);
-		//lblLeaderboard.addMouseListener(keys);
 		
 		JLabel lblTutorials = new JLabel("Tutorials");
+		lblTutorials.setForeground(Color.WHITE);
+		lblTutorials.setBounds(605, 408, 172, 29);
 		lblTutorials.setHorizontalAlignment(SwingConstants.CENTER);
 		lblTutorials.setFont(new Font("Tahoma", Font.PLAIN, 24));
-		lblTutorials.setBounds(837, 527, 172, 29);
 		add(lblTutorials);
 		labels.add(lblTutorials);
-		//lblTutorials.addMouseListener(keys);
 		
 		JLabel lblCredits = new JLabel("Credits");
+		lblCredits.setForeground(Color.WHITE);
+		lblCredits.setBounds(602, 469, 172, 29);
 		lblCredits.setHorizontalAlignment(SwingConstants.CENTER);
 		lblCredits.setFont(new Font("Tahoma", Font.PLAIN, 24));
-		lblCredits.setBounds(837, 576, 172, 29);
 		add(lblCredits);
 		labels.add(lblCredits);
-		//lblCredits.addMouseListener(keys);
 		
 		JLabel lblExit = new JLabel("Exit");
+		lblExit.setForeground(Color.WHITE);
+		lblExit.setBounds(603, 523, 172, 29);
 		lblExit.setHorizontalAlignment(SwingConstants.CENTER);
 		lblExit.setFont(new Font("Tahoma", Font.PLAIN, 24));
-		lblExit.setBounds(837, 622, 172, 29);
 		add(lblExit);
 		labels.add(lblExit);
-		//lblExit.addMouseListener(keys);;
 		
 		JButton btnEasy = new JButton("Easy");
+		btnEasy.setBounds(536, 222, 75, 75);
 		btnEasy.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		btnEasy.setBounds(800, 344, 75, 75);
 		add(btnEasy);
 		buttons.add(btnEasy);
-		//btnEasy.addMouseListener(keys);
 		
 		JButton btnNormal = new JButton("Normal");
+		btnNormal.setBounds(645, 222, 117, 75);
 		btnNormal.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		btnNormal.setBounds(937, 344, 97, 75);
 		add(btnNormal);
 		buttons.add(btnNormal);
-		//btnNormal.addMouseListener(keys);
 		
 		JButton btnHard = new JButton("Hard");
+		btnHard.setBounds(790, 222, 89, 75);
 		btnHard.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		btnHard.setBounds(1092, 344, 75, 75);
 		add(btnHard);
 		buttons.add(btnHard);
-		//btnHard.addMouseListener(keys);
 		
 	}
 	
@@ -129,6 +126,7 @@ public class MainMenu extends JPanel
     public void paintComponent(Graphics g) 
     {
         super.paintComponent(g);
+        g.drawImage(background, 0, 0, 1366, 768, null);
         for ( int i = 0; i < 3; i++ )
         {
         	if (buttonSelected == i )
@@ -153,109 +151,68 @@ public class MainMenu extends JPanel
         		labels.get(i).setForeground(Color.RED);
         	}
         	else
-        		labels.get(i).setForeground(Color.BLACK);
+        		labels.get(i).setForeground(Color.WHITE);
         }
     }
 	
-	public void keyPressed(KeyEvent e){
-    	keys.keyPressed(e);
-    }
-    
-    public void keyReleased(KeyEvent e){
-
-    	keys.keyReleased(e);
-    }
-	
-	// Inner Class MyKeysAdapter
-    public class MyMouse extends KeyAdapter 
-    {
-    	//System.out.println("Here4");
-        @Override
-        public void keyPressed (KeyEvent e) 
-        {
-        	/*
-        	System.out.println("HERE");
-        	if ( e.getSource() instanceof JLabel )
-        	{
-//        	if ( (JLabel) e.getSource()  == labels.get(0) )
-//        	{
-//        		console.selectOpt( 1, level + 1);
-//        	}
-//        	else if ( (JLabel) e.getSource() == labels.get(1) )
-//        	{
-//        		console.selectOpt( 2, level + 1);
-//        	}
-//        	if ( (JLabel) e.getSource() == labels.get(3) )
-//        	{
-//        		console.selectOpt( 3, -1);
-//        	}
-        	 if ( (JLabel) e.getSource() == labels.get(4) )
-        	{
-        		console.selectOpt( 4, -1);
-        	}
-        	}
-        	else if ( e.getSource() instanceof JButton)
-        	{
-        	 if ( (JButton) e.getSource() == buttons.get(0) )
-        	{
-        		level = 1;
-        	}
-        	else if ( (JButton) e.getSource() == buttons.get(1) )
-        	{
-        		level = 2;
-        	}
-        	else if ( (JButton) e.getSource() == buttons.get(2) )
-        	{
-        		level = 3;
-        	}
-        	}
-        	*/
-        	int keyCode = ((KeyEvent) e).getKeyCode();
-        	if ( keyCode == KeyEvent.VK_UP )
-        	{
-        		if (labelSelected > 0)
-        			labelSelected--;
-        	}
-        	else if ( keyCode == KeyEvent.VK_DOWN )
-        	{
-        		if (labelSelected < 6)
-        			labelSelected++;
-        	}
-        	else if ( keyCode == KeyEvent.VK_LEFT )
-        	{
-        		if (labelSelected == 2 )
-        		{
-        			if (buttonSelected > 0 )
-        				buttonSelected--;
+	public void keyTyped(KeyEvent e){}
+	public void keyReleased(KeyEvent e){}
+	public void keyPressed(KeyEvent e)
+	{
+		int keyCode = e.getKeyCode();
+        if ( keyCode == KeyEvent.VK_UP )
+       	{
+       		if (labelSelected > 0)
+       			labelSelected--;
+       	}
+       	else if ( keyCode == KeyEvent.VK_DOWN )
+       	{
+       		if (labelSelected < 6)
+       			labelSelected++;
+       	}
+       	else if ( keyCode == KeyEvent.VK_LEFT )
+       	{
+       		if (labelSelected == 2 )
+       		{
+       			if (buttonSelected > 0 )
+       				buttonSelected--;
+       		}
+       	}        	
+       	else if ( keyCode == KeyEvent.VK_RIGHT )
+       	{
+       		if (labelSelected == 2 )
+       		{
+       			if (buttonSelected < 2 )
+       				buttonSelected++;
+       		}
+       	}        	
+       	else if ( keyCode == KeyEvent.VK_ENTER )
+       	{
+       		if ( labelSelected == 2 )
+       		{
+       			level = buttonSelected;
+       		}
+       		if ( labelSelected == 6 )
+       		{
+       			new JOptionPane();
+				int selection = JOptionPane.showConfirmDialog(null,"Are you sure you want to exit", "Choose one", JOptionPane.YES_NO_OPTION);
+       			if ( selection == JOptionPane.YES_OPTION )
+       			{
+       				console.exit();
+       			}
+       		}
+       		else
+       		{
+        		if ( labelSelected < 2 )
+         		{
+         			console.selectOpt( labelSelected + 1 , level + 1);
         		}
-        	}
-        	else if ( keyCode == KeyEvent.VK_RIGHT )
-        	{
-        		System.out.println(labelSelected);
-        		if (labelSelected == 2 )
-        		{
-        			if (buttonSelected < 2 )
-        				buttonSelected++;
+        		if ( labelSelected > 2 )
+         		{
+        			console.selectOpt( labelSelected , -1);
         		}
-        	}
-        	else if ( keyCode == KeyEvent.VK_ENTER )
-        	{
-        		if ( labelSelected == 2 )
-        		{
-        			level = buttonSelected;
-        		}
-        		else
-        		{
-         			if ( labelSelected < 2 )
-         			{
-         				console.selectOpt( labelSelected + 1 , level + 1);
-         			}
-         			if ( labelSelected > 2 )
-         			{
-         				console.selectOpt( labelSelected , -1);
-         			}
-        		}
-        	}
-        	repaint();
-        }
-    }}
+       		}
+       	}
+        repaint();
+	}
+}
